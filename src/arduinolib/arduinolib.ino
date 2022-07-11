896f35c798d1291d273b990cf091ca9d8cfbf9eb
PedroCorcaque <pedrollcorc@gmail.com>
2022-07-11 08:51:42 +0700

    test_one

--- src/arduinolib/arduinolib.ino
+++ src/arduinolib/arduinolib.ino
@@ -14,50 +14,60 @@ enum motors : byte {
   JAW_CLOCKWISE, JAW_ANTICLOCKWISE
 };
 
+enum motores : byte {
+  EYELID, EYEBROWM, EYE, MOUTH
+};
+
 Motor motor[12];
 DynamixelProtocol dxl(BAUDRATE, ID);
-unsigned char adress;
+unsigned char address;
 unsigned char angle;
 
 void setup() {
   dxl.init();
   Serial.begin(BAUDRATE); //BAUDRATE
   Serial.flush();
+  
   motor[EYEBROW_HEIGHT_RIGHT].setMotorDefinitions(20, 0, 150); 
   motor[EYEBROW_HEIGHT_LEFT].setMotorDefinitions(20, 150, 0);
   motor[EYEBROW_ANGLE_RIGHT].setMotorDefinitions(50, 0, 100); 
   motor[EYEBROW_ANGLE_LEFT].setMotorDefinitions(50, 100, 0); 
-  motor[EYELID_UP_RIGHT].setMotorDefinitions(20, 75, 39); //OK    
-  motor[EYELID_UP_LEFT].setMotorDefinitions(20, 10, 45); //Ok
-  motor[EYELID_DOWN_RIGHT].setMotorDefinitions(20, 0, 65); //Zero eh abrir //Ok
-  motor[EYELID_DOWN_LEFT].setMotorDefinitions(20, 58, 15); //Ok
+  motor[EYELID_UP_RIGHT].setMotorDefinitions(20, 75, 39);   
+  motor[EYELID_UP_LEFT].setMotorDefinitions(20, 10, 45); 
+  motor[EYELID_DOWN_RIGHT].setMotorDefinitions(20, 0, 65); 
+  motor[EYELID_DOWN_LEFT].setMotorDefinitions(20, 58, 15); 
   motor[EYE_HORIZONTAL].setMotorDefinitions(40, 0, 100); 
   motor[EYE_VERTICAL].setMotorDefinitions(85, 0, 100);
   motor[JAW_CLOCKWISE].setMotorDefinitions(100, 42, 53);
   motor[JAW_ANTICLOCKWISE].setMotorDefinitions(0, 0, 100);
 }
 
-int angles2;
 
 void loop() {
-  /*if(Serial.available())
-  {
-     angles2 = Serial.parseInt();
-     Serial.print("Input: ");
-     Serial.println(angles2);
-     motor[JAW_CLOCKWISE].goTo(angles2);
-  }
-}*/
-//  dxl.checkMessages();
-//  
   dxl.checkMessages();
  
   if(dxl.instruction != DXL_NO_DATA && dxl.id == ID) {
     switch(dxl.instruction) {
       case DXL_WRITE_DATA:
-        adress = dxl.parameters[0];
+        address = dxl.parameters[0];
         angle = dxl.parameters[1];
-        switch(adress) {
+        Serial.print(angle);
+//        switch(address) {
+//          case EYELID:
+//            // angle has 4 bytes
+//            break;
+//          case EYEBROWM:
+//            // angle has 4 bytes
+//            break;
+//          case EYE:
+//            // angle has 2 bytes
+//            break;
+//          case MOUTH:
+//            // angle has 1 byte
+//          default:
+//            break;
+//        }
+        switch(address) {
           case EYEBROW_HEIGHT_RIGHT:
             motor[EYEBROW_HEIGHT_RIGHT].goTo(angle);
             break;
